afa9d4a2cee9e8f8db4b6f9be9230a02395deb8d
rademacher_luk@K2AC.LOCAL <rademacher_luk@K2AC.LOCAL>
2018-10-11 11:21:40 +0200

    WIP

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import view.Start;
+
+public class Main {
+	public static void main(String[] args) {
+	    // TODO code application logic here
+	    Start frame=new Start();
+	    frame.setTitle("Word Cloud");
+	    frame.setSize(1000, 620);
+	    frame.setResizable(false);
+	    frame.setLocation(50, 50);
+	    frame.setVisible(true);
+	}
+}
---
+++ src/view/Start.java
@@ -0,0 +1,5 @@
+package view;
+
+public class Start extends javax.swing.JFrame{
+
+}
