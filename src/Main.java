66a8b38c85aeee91d9e0000321d52ff1cfad9c64
rademacher_luk@k2ac.local <rademacher_luk@k2ac.local>
2018-10-11 12:27:46 +0200

    added Graphics2d Example

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
 import view.Start;
+import javax.swing.JPanel;
+import java.awt.*;
 
 public class Main {
 	public static void main(String[] args) {
 	    // TODO code application logic here
 	    Start frame=new Start();
-	    frame.setTitle("Word Cloud");
-	    frame.setSize(1000, 620);
-	    frame.setResizable(false);
-	    frame.setLocation(50, 50);
-	    frame.setVisible(true);
+	    
+	    JPanel panel = new GPanel();
+	    
+	    panel.setBackground(new java.awt.Color(255, 255, 255));
+	    
+	    frame.setContentPane(panel);
 	}
+}
+
+
+class GPanel extends JPanel {
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D gr = (Graphics2D) g; 
+        
+        gr.drawString("Graph", 50, 50);
+    }
 }
--- src/view/Start.java
+++ src/view/Start.java
@@ -2,4 +2,11 @@ package view;
 
 public class Start extends javax.swing.JFrame{
 
+	public Start() {
+	    this.setTitle("Start");
+	    this.setSize(1000, 620);
+	    this.setLocation(50, 50);
+	    this.setVisible(true);
+	}
+	
 }
