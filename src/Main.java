1d589c5f22685de82cd1de2b83c4abc3a521db72
mentzel_ste@k2ac.local <mentzel_ste@k2ac.local>
2018-11-07 09:32:45 +0100

    Graph panel update

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,48 @@
 import view.Start;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import library.GraphPanel;
+
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
 	public static void main(String[] args) {
 	    // TODO code application logic here
+		Random rnd=new Random();
 	    Start frame=new Start();
-	    
-	    JPanel panel = new GPanel();
-	    
-	    panel.setBackground(new java.awt.Color(255, 255, 255));
-	    
-	    frame.setContentPane(panel);
-	}
-}
-
-
-class GPanel extends JPanel {
-
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D gr = (Graphics2D) g; 
+	    List<Double> testScores=new ArrayList<Double>();
+	    for(double i=1;i<100;i++) {
+	    	testScores.add(null);
+	    }
+	    GraphPanel grPanel= new GraphPanel(testScores);
+	    grPanel.setPreferredSize(new Dimension(800, 600));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(grPanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
         
-        gr.drawString("Graph", 50, 50);
-    }
+        while(true) 
+        {        	        	       	
+        		if(testScores.size()>100)
+            	{
+            		testScores.remove(0) ;
+            	}
+        		testScores.add(rnd.nextDouble());
+        	
+        	grPanel.setScores(testScores);    
+        	try {
+        		Thread.sleep(50);
+				} 
+        	catch (InterruptedException e) 
+	    		{
+        		e.printStackTrace();
+	    		}
+        }
+	}
 }
